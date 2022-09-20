9372e2dce12a77942f4b9477bc95451451ac868c
Birendra <divyabiru27@gmail.com>
2022-09-20 22:40:12 +0530

    Corrected by Priyanka

--- prime.js
+++ prime.js
@@ -1,18 +1,18 @@
 function CheckPrime(n){
     if(n<=1){
-        return true;
+        return false;
     }
     for(let i=2;i<=n**0.5;++i){
         if(n%i==0){
             return false;
         }
     }
-    return false;
+    return true;
 }
 let N=13;
 let result=CheckPrime(N);
 if(result){
-    console.log("Not Prime");
-}else{
     console.log("Prime");
+}else{
+    console.log(" Not Prime");
 }
